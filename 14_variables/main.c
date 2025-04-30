2c067bf0a6a7f3f7a35d7f69e17577fed301f925
Brian <bmcruzaz@gmail.com>
2025-04-30 02:37:51 -0300

    Agrego resolucion

---
+++ 14_variables/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int var = 10;
+
+int main() {
+    int var = 24;
+    printf("Variable global = %d \n", var);
+    printf("Variable local = %d \n", var);
+    return 0;
+}
+
+/*
+sucede que la variable global es ocultada/opacada por la variable local
+pues la variable global tiene "file scope" mientras que la local "block scope"
+y como estamos imprimiendo dentro de el bloque donde fue inicializada la variable local
+el scope actual es "block", por lo tanto el valor de var sera 24
+*/
