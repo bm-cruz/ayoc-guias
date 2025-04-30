e98b0e1fe3c02de101c203db88bb3f1051565100
Brian <bmcruzaz@gmail.com>
2025-04-30 04:46:57 -0300

    Agrego resolucion

---
+++ 18_estado_v3/main.c
@@ -0,0 +1,39 @@
+# include <stdio.h>
+# define FELIZ 0
+# define TRISTE 1
+
+int estado = TRISTE;  // tiene file scope y static duration
+
+void alcoholizar();
+void print_estado();
+
+int main() {
+    print_estado();                               // imprime "Estoy triste"
+    alcoholizar();
+    print_estado();                               // imprime "Estoy feliz"
+    alcoholizar(); alcoholizar(); alcoholizar();
+    print_estado();                               // imprime "Estoy triste"
+}
+
+void alcoholizar() {
+    int cantidad = 0;  // static duration. block scope
+    cantidad++;
+    if (cantidad < 3) {
+        estado = FELIZ;
+    } else {
+        estado = TRISTE;
+    }
+}
+
+void print_estado() {
+    printf("Estoy %s\n", estado == FELIZ ? "feliz" : "triste");
+}
+
+/*
+El programa imprime "Estoy triste", luego "Estoy feliz" y por ultimo "Estoy triste"
+Si se le quita la palabra clave "static" a la variable cantidad, esta pasara de tener
+"static duration" a "automatic duration",
+esto significa que el tiempo durante el cual esta variable
+tendra un espacio de memoria asignado sera modificado,
+pasara de existir en todo el programa, a solo existir dentro de la funcion "alcoholizar"
+*/
